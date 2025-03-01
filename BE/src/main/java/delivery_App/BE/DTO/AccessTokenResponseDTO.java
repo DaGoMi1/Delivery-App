65bcd520e43703ec05366352f31d7f32b1479f2e
DaGeom Lee <dlekrja4523@gmail.com>
2025-03-01 13:39:03 +0900

    g

--- BE/src/main/java/delivery_App/BE/DTO/AccessTokenResponseDTO.java
+++
@@ -1,8 +0,0 @@
-package delivery_App.BE.DTO;
-
-import lombok.Data;
-
-@Data
-public class AccessTokenResponseDTO {
-    private String accessToken;
-}
--- BE/src/main/java/delivery_App/BE/DTO/LoginDTO.java
+++
@@ -1,9 +0,0 @@
-package delivery_App.BE.DTO;
-
-import lombok.Data;
-
-@Data
-public class LoginDTO {
-    private String userId;
-    private String password;
-}
